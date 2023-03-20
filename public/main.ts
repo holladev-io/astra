8dbaa25c522e6cfe1c62e4dc7d102a585b4ee38b
Holla Dev <holladev.io@gmail.com>
2023-03-20 22:19:13 +0200

    fix wf

--- public/main.ts
+++ public/main.ts
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/std@0.167.0/http/server.ts";
-import { handle } from '../dist/server/entry.mjs';
+import { handle } from '../server/entry.mjs';
 
 serve((req: Request) => {
   // Check the request, maybe do static file handling here.
